cf438ed3c046a4063c2d5ddae37e653d1fae1a3b
Piotr Pszczółkowski <piotr@beesoft.pl>
2025-12-01 13:31:46 +0100

    comments

--- server/handler.cpp
+++ server/handler.cpp
@@ -1,9 +1,32 @@
+// MIT License
 //
-// Created by Piotr Pszczolkowski on 29/11/2025.
+// Copyright (c) 2024 Piotr Pszczółkowski
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Author: Piotr Pszczółkowski
+// Date: 29.11.2025
+// E-mail: piotr@beesoft.pl.
 
+/*------- include files:
+-------------------------------------------------------------------*/
 #include "handler.h"
-#include "../shared4cx/types.h"
 #include "../shared4cx/shared.h"
 #include "../sqlite4cx/sqlite4cx.hpp"
 #include <ranges>
@@ -18,7 +41,7 @@ namespace bee {
 
     Response handleRequest(Request &&request) {
         switch (request.type) {
-            case RequestType::Database:
+            case Database:
                 return handleDatabaseRequest(std::move(request));
             default:
                 return Response{.code = -1, .message = "Request type is not supported"};
@@ -27,6 +50,8 @@ namespace bee {
 
     Response handleDatabaseRequest(Request&& request) {
         switch (request.subType) {
+
+            //------- OPEN ------------------------------------------
             case Open: {
                 auto const name = request.value;
                 if (auto home = homeDirectory()) {
@@ -43,6 +68,8 @@ namespace bee {
                 }
                 return Response{.id = request.id, .code = -1, .message = "Failed to get home directory"};
             }
+
+            //------- CREATE ----------------------------------------
             case Create: {
                 auto const name = request.value;
                 if (auto home = homeDirectory()) {
